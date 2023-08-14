a22f4c1e9b379ef34043dbc25dda0fb452ec1102
Ashley <asyan77@gmail.com>
2023-08-13 19:10:46 -0700

    create frontend utils files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { restoreSession } from './utils/authUtils';
 
+// const store = configureStore({
+//   session: {
+//     currentUser: 1
+//   }
+// })
 
-ReactDOM.createRoot(document.getElementById('root'))
-.render(
-  <React.StrictMode>
-    {/* <Provider> */}
-      <App />
-    {/* </Provider> */}
-</React.StrictMode>,
-)
+const currentUser = sessionStorage.getItem('currentUser')
+const csrfToken = sessionStorage.getItem('csrfToken')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render (
+    <React.StrictMode>
+       {/* <Provider store= {store}>  */}
+        <App />
+       {/* </Provider>  */}
+  </React.StrictMode>
+  )
+}
+
+if (!currentUser || !csrfToken) {
+  restoreSession().then(renderApp)
+} else {
+  renderApp()
+}

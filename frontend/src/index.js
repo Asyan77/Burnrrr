d7e5482e91018c2dd518b3561c426dd2c0d2fdb6
Ashley <asyan77@gmail.com>
2023-08-13 20:00:16 -0700

    create usersReducer.js, store.js, sessionsReducer.js, entitiesReducer.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
 import './index.css';
 import App from './App';
 import { restoreSession } from './utils/authUtils';
-
-// const store = configureStore({
-//   session: {
-//     currentUser: 1
-//   }
-// })
+import { deleteSession, postSession, postUser } from './utils/sessionApiUtils';
+import { createUser, loginUser, logoutUser } from './store/usersReducer';
 
 const currentUser = sessionStorage.getItem('currentUser')
 const csrfToken = sessionStorage.getItem('csrfToken')
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+let initialState = {}
+const currentUserData = JSON.parse(currentUser)
+
+if (currentUserData) {
+  initialState = {
+    entities: {
+      users: {
+        [currentUserData.id]: currentUserData
+      }
+    },
+    session: {
+      currentUser: currentUserData.id
+    }
+  }
+}
+
+const store = configureStore(initialState)
+
+
+
+
 const renderApp = () => {
   root.render (
     <React.StrictMode>
-       {/* <Provider store= {store}>  */}
+       <Provider store= {store}> 
         <App />
-       {/* </Provider>  */}
+       </Provider> 
   </React.StrictMode>
   )
 }
@@ -29,3 +48,19 @@ if (!currentUser || !csrfToken) {
 } else {
   renderApp()
 }
+
+
+// for testing only
+window.store = store
+// window.receiveTea = receiveTea
+// window.receiveTeas = receiveTeas
+// window.removeTea = removeTea
+// window.TeaApiUtils = TeaApiUtils
+// window.fetchTeas = fetchTeas
+window.postUser = postUser
+window.postSession = postSession
+window.deleteSession = deleteSession
+window.loginUser = loginUser
+window.logoutUser = logoutUser
+window.createUser = createUser
+//
